d341dfbdeb0b4b7ccb3b7201899dd9d435b56451
munjeni <sharecard17@gmail.com>
2013-04-02 18:35:13 +0200

    fix helper script: prevent importing default strings to the translations folder

--- tools/prepare_translations_and_copy_to_the_translations_folder.sh
+++ tools/prepare_translations_and_copy_to_the_translations_folder.sh
@@ -5,9 +5,11 @@ do
 	if [ -d /root/miui_sony_xperia/multilang/system/app/$LIST/res ]; then
 		for VALUESLANG in `ls /root/miui_sony_xperia/multilang/system/app/$LIST/res`
 		do
-			if [ -f /root/miui_sony_xperia/multilang/system/app/$LIST/res/$VALUESLANG/strings.xml ]; then
-				[ ! -d /root/miui_sony_xperia/translations/$LIST/res/$VALUESLANG ] && mkdir -p /root/miui_sony_xperia/translations/$LIST/res/$VALUESLANG
-				cp -fr /root/miui_sony_xperia/multilang/system/app/$LIST/res/$VALUESLANG/strings.xml /root/miui_sony_xperia/translations/$LIST/res/$VALUESLANG/strings.xml
+			if [ "$VALUESLANG" != "values" ]; then
+				if [ -f /root/miui_sony_xperia/multilang/system/app/$LIST/res/$VALUESLANG/strings.xml ]; then
+					[ ! -d /root/miui_sony_xperia/translations/$LIST/res/$VALUESLANG ] && mkdir -p /root/miui_sony_xperia/translations/$LIST/res/$VALUESLANG
+					cp -fr /root/miui_sony_xperia/multilang/system/app/$LIST/res/$VALUESLANG/strings.xml /root/miui_sony_xperia/translations/$LIST/res/$VALUESLANG/strings.xml
+				fi
 			fi
 		done
 	fi 

946bbd22091c2f97dea0e2fd0c60d3f06d060746
munjeni <sharecard17@gmail.com>
2013-04-02 18:36:50 +0200

    enable translations for test only to the some apks

--- stock_lotus/customize_miui_app.sh
+++ stock_lotus/customize_miui_app.sh
@@ -17,33 +17,73 @@ fi
 
 if [ $1 = "MiuiSystemUI" ];then
 	$XMLMERGYTOOL $1/res/values $2/res/values
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
 fi
 
 if [ $1 = "Mms" ];then
 	$XMLMERGYTOOL $1/res/values $2/res/values
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
 fi
 
 if [ $1 = "MiuiHome" ];then
 	$XMLMERGYTOOL $1/res/values $2/res/values
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
 fi
 
 if [ $1 = "Settings" ];then
 	$XMLMERGYTOOL $1/res/values $2/res/values
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
 fi
 
 if [ $1 = "Music" ];then
 	$XMLMERGYTOOL $1/res/values $2/res/values
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
 fi
 
 if [ $1 = "Phone" ];then
     $XMLMERGYTOOL $1/res/values $2/res/values
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
 fi
 
 if [ $1 = "MiuiGallery" ];then
     $XMLMERGYTOOL $1/res/values $2/res/values
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
 fi
 
 if [ $1 = "ThemeManager" ];then
     $XMLMERGYTOOL $1/res/values $2/res/values
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
 fi
 

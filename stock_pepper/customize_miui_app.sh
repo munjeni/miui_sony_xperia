ed27718207f33af28465cff9c6d9b0f0daacf9b0
munjeni <sharecard17@gmail.com>
2013-04-02 21:57:46 +0200

    enable multilang for all apks (only strings.xml for now), add missing strings to the: Calendar.apk LBESEC_MIUI.apk Stk.apk

--- stock_pepper/customize_miui_app.sh
+++ stock_pepper/customize_miui_app.sh
@@ -7,13 +7,6 @@
 XMLMERGYTOOL=$PORT_ROOT/tools/ResValuesModify/jar/ResValuesModify
 echo "original dir: $2"
 echo "target dir:$1"
-if [ $1 = "AntiSpam" ]; then
-       for LIST in `ls $PORT_ROOT/translations/$1/res/`
-       do
-               [ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
-               php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
-       done
-fi
 
 if [ $1 = "MiuiSystemUI" ];then
 	$XMLMERGYTOOL $1/res/values $2/res/values
@@ -87,3 +80,282 @@ if [ $1 = "ThemeManager" ];then
        done
 fi
 
+if [ $1 = "AntiSpam" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "Backup" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "Browser" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "BugReport" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "Calculator" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "Calendar" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "CalendarProvider" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "ChromeBookmarksSyncAdapter" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "CloudService" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "Contacts" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "ContactsProvider" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "DeskClock" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "DownloadProvider" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "DownloadProviderUi" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "FileExplorer" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "LBESEC_MIUI" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "MiuiCompass" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "NetworkLocation" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "Notes" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "PackageInstaller" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "QuickSearchBox" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "SettingsProvider" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "SoundRecorder" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "TelephonyProvider" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "TelocationProvider" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "Updater" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "Stk" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "Bluetooth" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "WeatherProvider" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "GuardProvider" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "NetworkAssistant" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "XiaomiServiceFramework" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "MiuiVideoPlayer" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "VpnDialogs" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
+
+if [ $1 = "AirkanPhoneService" ]; then
+	for LIST in `ls $PORT_ROOT/translations/$1/res/`
+	do
+		[ ! -d $2/res/$LIST ] && mkdir $2/res/$LIST
+		php -f $PORT_ROOT/tools/prevod.php a=$2/res/values/strings.xml b=$PORT_ROOT/translations/$1/res/$LIST/strings.xml c=$2/res/$LIST/strings.xml
+	done
+fi
